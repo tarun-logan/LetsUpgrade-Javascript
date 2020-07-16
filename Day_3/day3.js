23f47a43f697ce99db62abbbf299c1ffdc487d9a
Tarun Nagaraj <62003943+tarun-logan@users.noreply.github.com>
2020-07-16 23:05:40 +0530

    Update day3.js

--- Day_3/day3.js
+++ Day_3/day3.js
@@ -1,3 +1,9 @@
 let oddEve = function(num) {
     return num % 2;
 }
+
+let fetchOs = function () {
+    let os = window.prompt('Enter your OS name and it\'s version (leave a space inbetween)'); 
+    let arr = os.split(' ');   
+    return arr;
+}
